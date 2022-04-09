27bf98d24d24a2c2e22bbacedd7126a527569799
Ernando <ernandofvjr@hotmail.com>
2022-04-09 16:32:07 -0300

    Adiciona validações nos endpoints de User

--- authuser_microservice/src/main/java/com/ead/authuser/dtos/UserDto.java
+++ authuser_microservice/src/main/java/com/ead/authuser/dtos/UserDto.java
@@ -2,6 +2,12 @@ package com.ead.authuser.dtos;
 
 import java.util.UUID;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.br.CPF;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonView;
 
@@ -21,15 +27,23 @@ public class UserDto {
 	
 	private UUID userId;
 	
+	@NotBlank(groups = UserView.RegistrationPost.class)
+	@Size(min = 4, max = 50, groups = UserView.RegistrationPost.class)
 	@JsonView(UserView.RegistrationPost.class)
 	private String username;
 	
+	@NotBlank(groups = UserView.RegistrationPost.class)
+	@Email(groups = UserView.RegistrationPost.class)
 	@JsonView(UserView.RegistrationPost.class)
 	private String email;
 	
+	@NotBlank(groups = {UserView.RegistrationPost.class, UserView.PasswordPut.class})
+	@Size(min = 6, max = 20, groups = {UserView.RegistrationPost.class, UserView.PasswordPut.class})
 	@JsonView({UserView.RegistrationPost.class, UserView.PasswordPut.class})
 	private String password;
 	
+	@NotBlank(groups = UserView.PasswordPut.class)
+	@Size(min = 6, max = 20, groups = UserView.PasswordPut.class)
 	@JsonView(UserView.PasswordPut.class)
 	private String oldPassword;
 	
@@ -40,8 +54,10 @@ public class UserDto {
 	private String phoneNumber;
 	
 	@JsonView({UserView.RegistrationPost.class, UserView.Userput.class})
+	@CPF(groups = {UserView.RegistrationPost.class, UserView.Userput.class})
 	private String cpf;
 	
+	@NotBlank(groups = UserView.ImagePut.class)
 	@JsonView(UserView.ImagePut.class)
 	private String imageUrl;
 
