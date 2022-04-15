287069892ff30e35bbd6cf8c30afa01a896e6191
Ernando <ernandofvjr@hotmail.com>
2022-04-15 15:29:21 -0300

    Adiciona specifications na consulta getAllUsers
    -mudando para java 11 o problema de dependência foi resolvido

--- authuser_microservice/src/main/java/com/ead/authuser/controllers/UserController.java
+++ authuser_microservice/src/main/java/com/ead/authuser/controllers/UserController.java
@@ -25,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ead.authuser.dtos.UserDto;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
+import com.ead.authuser.specifications.SpecificationTemplate;
 import com.fasterxml.jackson.annotation.JsonView;
 
 
@@ -39,9 +40,10 @@ public class UserController {
 	
 	
 	@GetMapping
-	public ResponseEntity<Page<UserModel>> getAllUserts(@PageableDefault(page = 0, size = 10, sort = "userId", direction = Sort.Direction.ASC) Pageable pageable) {
+	public ResponseEntity<Page<UserModel>> getAllUserts(SpecificationTemplate.UserSpec spec,
+														@PageableDefault(page = 0, size = 10, sort = "userId", direction = Sort.Direction.ASC) Pageable pageable) {
 		
-		Page<UserModel> userModelPage = userService.findAll(pageable);
+		Page<UserModel> userModelPage = userService.findAll(spec, pageable);
 		return ResponseEntity.status(HttpStatus.OK).body(userModelPage);
 	}
 	
